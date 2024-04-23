9306eaf69b00f77865114d53eb7e0decfdbd48be
Luis Mendes <60905783+luisfmendes@users.noreply.github.com>
2024-04-23 16:49:34 -0300

    Add files via upload

---
+++ SymbolSelectorControl.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Sistema_Integrado.SeekClass
+{
+    partial class SymbolSelectorControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.symbolBox = new DevComponents.DotNetBar.Controls.SymbolBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // symbolBox
+            // 
+            this.symbolBox.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.symbolBox.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.symbolBox.Location = new System.Drawing.Point(3, 3);
+            this.symbolBox.Name = "symbolBox";
+            this.symbolBox.Size = new System.Drawing.Size(36, 31);
+            this.symbolBox.TabIndex = 0;
+            this.symbolBox.Text = "symbolBox1";
+            this.symbolBox.Click += new System.EventHandler(this.symbolBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(144, 111);
+            this.panel1.TabIndex = 1;
+            this.panel1.Visible = false;
+            // 
+            // SymbolSelectorControl
+            // 
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.symbolBox);
+            this.Name = "SymbolSelectorControl";
+            this.Size = new System.Drawing.Size(42, 37);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        public DevComponents.DotNetBar.Controls.SymbolBox symbolBox;
+    }
+}
---
+++ SymbolSelectorControl.cs
@@ -0,0 +1,130 @@
+﻿using DevComponents.DotNetBar;
+using DevComponents.DotNetBar.Controls;
+using DevComponents.DotNetBar.Rendering;
+using DevExpress.Utils.DirectXPaint;
+using FastReport.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace Sistema_Integrado.SeekClass
+{
+
+    public partial class SymbolSelectorControl : UserControl
+    {
+        private const int UIntSize = 4;
+        private static readonly IntPtr HGDI_ERROR = new IntPtr(-1);
+
+
+        public SymbolSelectorControl()
+        {
+            InitializeComponent();
+        }
+
+
+        private void SetColors(ItemPanel itemPanel)
+        {
+            ElementStyle styleClass = (ElementStyle)((Office2007Renderer)GlobalManager.Renderer).ColorTable.StyleClasses[(object)ElementStyleClassKeys.ItemPanelKey];
+            itemPanel.BackgroundStyle.BackColor = styleClass.BackColor;
+            itemPanel.BackgroundStyle.BackColor2 = styleClass.BackColor2;
+        }
+
+        [DllImport("gdi32.dll")]
+        private static extern uint GetFontUnicodeRanges(IntPtr hdc, IntPtr lpgs);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+
+        private void symbol_Click(object sender, EventArgs e)
+        {
+            if ((sender as ItemPanel).SelectedItem != null)
+            {
+                symbolBox.Symbol = (sender as ItemPanel).SelectedItem.ToString();
+                diminuiExpandeObjeto();
+            }
+        }
+
+        private void symbolBox1_Click(object sender, EventArgs e)
+        {
+            if (panel1.Controls.Count <= 0)
+            {                
+                string str = (string)null;
+                System.Windows.Forms.TabControl tabControl = new System.Windows.Forms.TabControl();
+                tabControl.TabPages.Add("Símbolos");
+                tabControl.Height = 200;
+                ItemPanel itemPanel1 = new ItemPanel();
+                itemPanel1.Font = Symbols.FontAwesome;
+                itemPanel1.MultiLine = true;
+                itemPanel1.AutoScroll = true;
+                itemPanel1.LayoutOrientation = eOrientation.Horizontal;
+                itemPanel1.Dock = DockStyle.Fill;
+                itemPanel1.SelectedIndexChanged += symbol_Click;
+                tabControl.TabPages[0].Controls.Add((Control)itemPanel1);
+                this.SetColors(itemPanel1);
+                for (int index = 61440; index <= 61952; ++index)
+                {
+                    ButtonItem buttonItem = new ButtonItem();
+                    buttonItem.AutoCheckOnClick = true;
+                    buttonItem.OptionGroup = "sym";
+                    buttonItem.Text = char.ConvertFromUtf32(index);
+                    buttonItem.Tooltip = string.Format("{0:x}", (object)index);
+                    buttonItem.Tag = (object)index;
+                    if (buttonItem.Text == str)
+                        buttonItem.Checked = true;
+                    itemPanel1.Items.Add((BaseItem)buttonItem);
+                }
+
+                eSymbolSet eSymbolSet = eSymbolSet.Awesome;
+                string name = this.Name;
+                PropertyInfo property = this.GetType().GetProperty(name + "Set", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                if (property != null && property.PropertyType == typeof(eSymbolSet))
+                    eSymbolSet = (eSymbolSet)property.GetValue(this, (object[])null);
+                tabControl.SelectedIndex = eSymbolSet != eSymbolSet.Material ? 0 : 1;
+                tabControl.Dock = DockStyle.Fill;
+                panel1.Controls.Clear();
+                panel1.Controls.Add((Control)tabControl);
+                panel1.Visible = true;
+                panel1.Size = new Size(230, 160);
+                this.Size = new Size(240, 200);
+                this.BringToFront();
+            }
+            else
+            {
+                diminuiExpandeObjeto();
+            }
+        }
+
+        private void diminuiExpandeObjeto()
+        {
+            try
+            {
+                panel1.Visible = !panel1.Visible;
+                if (panel1.Visible)
+                {
+                    this.BringToFront();
+                    this.Size = new Size(240, 200);
+                }
+                else
+                {
+                    this.Size = new Size(40, 35);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
